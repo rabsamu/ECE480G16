f0d92700cc3cce136fa0510f1929c019e8abf16c
rabsamu <rabsamu@gmail.com>
2025-03-13 19:53:12 -0400

    Make demo code

--- controller_test/controller_test.ino
+++ controller_test/controller_test.ino
@@ -1,4 +1,4 @@
-#define HWSERIAL Serial1;
+#define HWSERIAL Serial1
 
 // Motor A connections
 int enA = 9;
@@ -9,8 +9,9 @@ int enB = 3;
 int in3 = 5;
 int in4 = 4;
 
+int directionA = 0;
+int directionB = 0;
 
-int state = 0;
 
 void setup() {
   // Set all the motor control pins to outputs
@@ -21,35 +22,50 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   
-  // Turn off motors - Initial state
+  // Set motors to forwrd - Initial state
   digitalWrite(enA, 0);
   digitalWrite(enB, 0);
   digitalWrite(in1, 1);
   digitalWrite(in2, 0);
-  digitalWrite(in3, 0);
+  digitalWrite(in3, 1);
   digitalWrite(in4, 0);
 }
 
 void loop() {
+  if(HWSERIAL.available()) {
+    char msg = HWSERIAL.read();
+    switch(msg) {
+      case '1':
+        gotoState1();
+        break;
+      case '2':
+        gotoState2();
+        break;
+      case '3':
+        gotoState3();
+        break;
+      case '4':
+        toggleDirection(1);
+        toggleDirection(2);
+        break;
+    }
+  }
+  delay(10);
+}
 
-  speedCycle();
-  delay(2000);
-//  switch(state) {
-//    case 0:
-//      if(HWSERIAL.Available()) {
-//        speedControl(1, 80);
-//        speedcontrol(2, 0);
-//        case = 1;
-//      }
-//      break;
-//    case 1:
-//      if(HWSERIAL.Available()) {
-//        speedControl(1, 0);
-//        speedcontrol(2, 80);
-//        case = 1;
-//      }
-//  }
-//  delay(20);
+void gotoState1() {
+  speedControl(1, 80);
+  speedControl(2, 0);
+}
+
+void gotoState2() {
+  speedControl(1, 0);
+  speedControl(2, 80);
+}
+
+void gotoState3() {
+  speedControl(1, 0);
+  speedControl(2, 0);
 }
 
 void speedControl(int motor, int percent) {
@@ -61,36 +77,32 @@ void speedControl(int motor, int percent) {
   }
 }
 
-void directionControl() {
-  // Set motors to maximum speed
-  // For PWM maximum possible values are 0 to 255
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+void setDirection(int motor, int dir) {
+  if (motor== 1) {
+    directionA = dir;
+    digitalWrite(in1, directionA);
+    digitalWrite(in2, 1-directionA);
+  } else {
+    directionB = dir;
+    digitalWrite(in3, directionB);
+    digitalWrite(in4, 1-directionB);
+  }
+}
 
-  // Turn on motor A & B
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  delay(2000);
-  
-  // Now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  delay(2000);
-  
-  // Turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+void toggleDirection(int motor) {
+  if (motor == 1) {
+    directionA = 1 - directionA;
+    digitalWrite(in1, directionA);
+    digitalWrite(in2, 1-directionA);
+  } else {
+    directionB = 1 - directionB;
+    digitalWrite(in3, directionB);
+    digitalWrite(in4, 1-directionB);
+  }
 }
 
 // This function lets you control speed of the motors
 void speedCycle() {
-  
   // Accelerate from zero to maximum speed
   for (int i = 50; i < 100; i++) {
     speedControl(1, i);
